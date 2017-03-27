526dc65455048a73451b327965ef18bf7036a817
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-27 15:27:22 -0400

    initiate users controller and view

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,5 +2,12 @@ class HousesController < ApplicationController
 
 	def show
 		@house = House.find(params[:id])
+		if current_user && current_user.houses.includes(@house)
+			@permission = true
+			@members = @house.users
+		else
+			@alert = "Sorry, you must be a member of this household to view this page."
+			@permission = false
+		end
 	end
 end

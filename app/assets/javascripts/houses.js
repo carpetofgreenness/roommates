c06a8bc5cffd6d645087352ae6b0688cbb3fd5ed
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-29 16:54:18 -0400

    calculates balance and displays the list a little nicer

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -3,7 +3,6 @@ $("document").ready(function(){
 	// when user clicks an option, add it to the form and submit
 	$(".list-group-item").click(function(event){
 		var x = event.target.id.toString()
-		console.log("the number is " + x)
 		$("#item-info"+x).toggle();
 	})
 })

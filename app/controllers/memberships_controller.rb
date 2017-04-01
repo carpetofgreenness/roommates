968cbb1895539021e819c37775508a5ee6cb36a9
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-01 16:18:03 -0400

    gotta take a break, but working on making it so you can only join a house once. Also gotta take out turbolinks because this is silly

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,14 +1,26 @@
 class MembershipsController < ApplicationController
 
 	def create
-		house = House.find(params[:membership][:house_id])
-		if house && params[:join_key] == house.join_key && house.is_member?(params[:membership][:user_id])
+		house_id = params[:membership][:house_id]
+		house = House.find_by_id(house_id)
+		user_id = params[:membership][:user_id]
+		p "WILL IT LET ME JOIN?"
+		p "the user is #{params[:membership][:user_id]}"
+		p "the house is #{house_id}"
+		p "is this user a member? #{house.is_member?(user_id)}" if house
+		if !house
+			flash[:notice] = "Sorry, we could not find a house with id #{house_id}"
+			redirect_to "/"
+		elsif params[:join_key] != house.join_key
+			flash[:notice] = "Sorry, that join key did not match the house with id #{house_id}"
+			redirect_to "/"
+		elsif house.is_member?(user_id )
+			flash[:notice] = "You are already a member of the house with the id #{house_id}, it is #{house.name}"
+			redirect_to "/"
+		else
 			Membership.create(membership_params)
 			flash[:notice] = "You successfully joined #{house.name}!"
 			redirect_to house
-		else
-			flash[:notice] = "Sorry, you were not able to join that house"
-			redirect_to "/"
 		end
 	end
 
--- app/models/house.rb
+++ app/models/house.rb
@@ -6,8 +6,8 @@ class House < ApplicationRecord
 	has_many :items, dependent: :destroy
 	has_many :recurring, dependent: :destroy
 
-	def is_member?(user_id)
-		self.memberships.map(&:user_id).include?(user_id)
+	def is_member?(user_id_num)
+		self.memberships.map(&:user_id).include?(user_id_num)
 	end
 
 end

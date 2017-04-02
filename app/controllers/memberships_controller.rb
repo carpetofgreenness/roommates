5b5ee8f367be84de12f002626901884b34fff334
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-01 20:52:38 -0400

    user can only join a house once. but why can't it submit the modal unless refresh?

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,14 +7,14 @@ class MembershipsController < ApplicationController
 		p "WILL IT LET ME JOIN?"
 		p "the user is #{params[:membership][:user_id]}"
 		p "the house is #{house_id}"
-		p "is this user a member? #{house.is_member?(user_id)}" if house
+		p "is this user a member? #{house.is_member?(2)}" if house
 		if !house
 			flash[:notice] = "Sorry, we could not find a house with id #{house_id}"
 			redirect_to "/"
 		elsif params[:join_key] != house.join_key
 			flash[:notice] = "Sorry, that join key did not match the house with id #{house_id}"
 			redirect_to "/"
-		elsif house.is_member?(user_id )
+		elsif house.is_member?(user_id.to_i)
 			flash[:notice] = "You are already a member of the house with the id #{house_id}, it is #{house.name}"
 			redirect_to "/"
 		else

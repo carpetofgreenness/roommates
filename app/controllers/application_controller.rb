d8b5edb13113878e24bcdba208cf8cac903edf03
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-03 17:13:27 -0400

    fix unpermitted params problem with devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,19 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
   end
 
-
-  protected
-  
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_session_path, :notice => 'if you want to add a notice'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
+  def authenticated_user!
+    store_origin_path
+    authenticate_user!
   end
+
+  
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   else
+  #     redirect_to new_user_session_path, :notice => 'if you want to add a notice'
+  #     ## if you want render 404 page
+  #     ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+  #   end
+  # end
 end

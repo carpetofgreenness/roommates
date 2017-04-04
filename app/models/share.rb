3e7589ef2709a7a01665503f03d89f5663e9e8c5
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-04-04 16:38:00 -0400

    make the repayment items charge everyone properly"

--- app/models/share.rb
+++ app/models/share.rb
@@ -4,10 +4,14 @@ class Share < ApplicationRecord
 
 	# calclate amount of share based on item total and number of users sharing
 	def amount
-		item = self.item
-		total = item.amount
-		split = item.users.count
-		amount = total/split
-		self.owner ? amount : -amount
+		if self.item.shared
+			item = self.item
+			total = item.amount
+			split = item.users.count
+			amount = total/split
+			self.owner ? amount : -amount
+		else
+			self.owner ? self.item.amount : -self.item.amount
+		end
 	end
 end
